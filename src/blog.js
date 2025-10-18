4b61d3798c5f2592b1c17ec537e24d55d1cb059d
Adonis <bigirimanaadonis@gmail.com>
2025-10-18 13:11:32 +0300

    CPMP

---
+++ src/blog.js
@@ -0,0 +1,21 @@
+const blogData = {
+  name: "My Blog",
+  image: "https://via.placeholder.com/215",
+  about: "A wonderful blog about React",
+  posts: [
+    {
+      id: 1,
+      title: "React is Fun",
+      date: "July 13, 2020",
+      preview: "Learning React is great!",
+    },
+    {
+      id: 2,
+      title: "Using Props",
+      date: "July 14, 2020",
+      preview: "Props help us pass data between components.",
+    },
+  ],
+};
+
+export default blogData;
